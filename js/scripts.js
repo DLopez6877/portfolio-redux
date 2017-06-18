196181026783c9fdf98050938b862e0dfe8c5b6e
Daniel <dlopez6877@gmail.com>
2017-06-17 21:16:38 -0700

    add animations. js apears abruptly -- fix later

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
   $(".hamburger").click(function(){
     $(this).toggleClass("is-active");
   });
+  $('.img-gradient')
+  .delay(3000)
+  .queue(function (next) {
+    $(this).css('opacity', '1');
+    next();
+  });
+  $('.js-img-gradient')
+  .delay(4000)
+  .queue(function (next) {
+    $(this).css('z-index', '-1');
+    next();
+  });
 });
